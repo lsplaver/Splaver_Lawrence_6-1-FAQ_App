ad3dc610fc90617789d5b67c5a280d4cfc4ed72e
Lawrence Splaver <lsplaver@yahoo.com>
2021-10-04 14:54:58 -0400

    Add project files.

---
+++ FAQapp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FAQapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
