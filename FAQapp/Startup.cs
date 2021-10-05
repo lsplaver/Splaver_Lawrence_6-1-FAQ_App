5e560a69a640cf7ac58d1fe86f5770c6e4aa161c
Lawrence Splaver <lsplaver@yahoo.com>
2021-10-05 15:35:52 -0400

    Created and seeded the database

--- FAQapp/Startup.cs
+++ FAQapp/Startup.cs
@@ -26,6 +26,8 @@ namespace FAQapp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<FAQContext>(options => options.UseSqlServer(Configuration.GetConnectionString("FAQContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
